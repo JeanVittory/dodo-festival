cf731dddc61659a04dae619f960e4634e2f70c61
Jean <vittory.dev@gmail.com>
2024-01-14 12:41:13 -0500

    Setting up responsiveness in the footer component

--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Sponsors(){
     return(
-        <article className="flex  justify-around items-center mt-20 bg-dodoWhite-200 w-full">
+        <article className="flex  justify-around  items-center mt-20 bg-dodoWhite-200 w-full flex-wrap">
             <Image src='/bbva.png' alt="logo bbva" width={3000} height={902}  className="w-36"></Image>
             <Image src='/cocacola.png' alt="logo cocacola" width={3000} height={902} className="w-36"></Image>
             <Image src='/kellogs.png' alt="logo kellogs" width={3000} height={902} className="w-36"></Image>
