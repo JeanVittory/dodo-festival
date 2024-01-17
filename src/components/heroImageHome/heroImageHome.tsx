a28af8c1fb0c4b21bbdcd7eb3491ed53930f7daa
Jean <vittory.dev@gmail.com>
2024-01-17 09:18:34 -0500

    The hero image was updated

--- src/components/heroImageHome/heroImageHome.tsx
+++ src/components/heroImageHome/heroImageHome.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function HeroImageHome(){
     return (
-        <section className="mt-16">
-            <Image src='/hero-image.png' alt="Festival flyer" width={940} height={850} className="w-[700px]"></Image>
+        <section className="mt-16 flex justify-center">
+            <Image src='/hero.png' alt="Festival flyer" width={940} height={850} className="w-[700px]"></Image>
         </section>
     )
 }
