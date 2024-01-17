7bf5f078a89e3aa1f89feb205843f38bc23802ce
Jean <vittory.dev@gmail.com>
2024-01-17 12:18:17 -0500

    Setting up favicon, title page and fixing an import issue of TicketButton component

--- src/components/heroImageHome/heroImageHome.tsx
+++ src/components/heroImageHome/heroImageHome.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import Button from "../button/Button";
+import TicketButton from "../button/TicketButton";
+
 
 export default function HeroImageHome(){
     return (
         <section className="mt-16 flex flex-col items-center">
             <Image src='/hero.png' alt="Festival flyer" width={940} height={950} className="w-[700px]"></Image>
-            <Button text='TICKETS'/>
+            <TicketButton text='TICKETS'/>
         </section>
     )
 }
