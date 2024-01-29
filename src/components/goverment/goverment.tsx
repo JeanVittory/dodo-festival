f116aae8e187d94bb5cdd663ee4c380e18323629
Jean <vittory.dev@gmail.com>
2024-01-29 12:08:26 -0500

    changing the route of whole images

--- src/components/goverment/goverment.tsx
+++ src/components/goverment/goverment.tsx
@@ -4,13 +4,13 @@ export default function Goverment (){
     return (
         <section className='flex gap-7'>
             <Image className='w-16 rounded-full' 
-                src="/static/alcaldia.jpg"
+                src="static/alcaldia.jpg"
                 alt="Logo alcaldia"
                 width={675}
                 height={599}
             />
             <Image className='w-16 rounded-full'
-                src="/static/secretaria.jpg"
+                src="static/secretaria.jpg"
                 alt="Logo secretaria"
                 width={1000}
                 height={1000}
--- src/components/heroImageHome/heroImageHome.tsx
+++ src/components/heroImageHome/heroImageHome.tsx
@@ -5,7 +5,7 @@ import TicketButton from "../button/TicketButton";
 export default function HeroImageHome(){
     return (
         <section className="mt-16 flex flex-col items-center">
-            <Image src='/static/hero.png' alt="Festival flyer" width={940} height={950} className="w-[700px]"></Image>
+            <Image src='static/hero.png' alt="Festival flyer" width={940} height={950} className="w-[700px]"></Image>
             <TicketButton text='TICKETS'/>
         </section>
     )
