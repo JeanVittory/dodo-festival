2e6f907ca999a552990fecbe5f490e8e1dc02493
Jean <vittory.dev@gmail.com>
2024-01-29 11:48:27 -0500

    Fixing static files routing and doing dynamic the basePath in next.config

--- src/components/label/label.tsx
+++ src/components/label/label.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Label() {
     return (
         <section className= {`${nunito800.className} text-dodoWhite-100 flex flex-col items-center text-sm`}>
-            <Image src='/logo-dodo.png' alt="logo dodo" width={70} height={70}/>
+            <Image src='/static/logo-dodo.png' alt="logo dodo" width={70} height={70}/>
             <div className="flex flex-col items-center mt-1 text-[10px]">
                 <p>CORPORACIÓN ARTISTICA</p>
                 <p>BUGA-VALLE</p>
