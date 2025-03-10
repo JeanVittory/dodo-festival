72b4643264e1a2cc691fa83160eaf497ae68b7b8
Kofi <107807110+Kof27@users.noreply.github.com>
2025-03-09 19:41:51 -0500

    Add age restriction field to event interface and display in festival dates, new festival dates included

--- src/interfaces/eventsFestival.ts
+++ src/interfaces/eventsFestival.ts
@@ -8,6 +8,7 @@ export interface IEventsFestival{
     place: string,
     address: string,
     description: string,
+    ageRestriction: string,
     type: string,
     picture: string,
     pictureAlt: string,
