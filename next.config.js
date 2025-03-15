198b8bdb965abd569155241ffb7e10b5da75dfb7
Kofi <107807110+Kof27@users.noreply.github.com>
2025-03-15 17:50:07 -0500

    Refactor BottonPushNotification component and adjust TicketButton margin

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     //output: "export",
-    reactStrictMode: true,
+    //reactStrictMode: true,
     //basePath: isProd ? '/dodo-festival' : '',
     images: {
         unoptimized: true,
--- src/components/bottonPushNotification/BottonPushNotiftication.tsx
+++ src/components/bottonPushNotification/BottonPushNotiftication.tsx
@@ -1,33 +1,36 @@
 "use client";
 
-import{useEffect} from 'react';
+import { useEffect } from 'react';
 import OneSignal from 'react-onesignal';
+import { nunito700 } from "@/lib/fonts";
 
-import { nunito700 } from "@/lib/fonts"
 const oneSignalID = process.env.NEXT_PUBLIC_ONE_SIGNAL_APP_ID;
 
-
-export default function Page() {
-    useEffect(() => {
-        // Ensure this code runs only on the client side
-        if (typeof window !== 'undefined') {
-          OneSignal.init({
-            appId: oneSignalID!,
-            // You can add other initialization options here
-            notifyButton: {
-              enable: true,
-            },
-            // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
-            allowLocalhostAsSecureOrigin: true
-          });
-        }
-      }, []);
-    
-      return (
-        <div>
-          <style jsx global>{`
-            ${nunito700}
-          `}</style>
-        </div>
-      );
+export default function BottonPushNotification() {
+  useEffect(() => {
+    // Ensure this code runs only on the client side
+    if (typeof window !== 'undefined') {
+      OneSignal.init({
+        appId: oneSignalID!,
+        // You can add other initialization options here
+        notifyButton: {
+          enable: true,
+        },
+        // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
+        // allowLocalhostAsSecureOrigin: true
+      });
     }
+  }, []);
+
+
+  return (
+    <section className='items-center justify-center flex flex-col'>
+      <div className="relative mt-10 mb-12 group">
+        <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-400 rounded-2xl blur-lg opacity-75 group-hover:opacity-100 transition duration-200"></div>
+        <button className={`${nunito700.className} text-dodoWhite-200 relative bg-dodoRed-100 py-2 px-5 rounded-2xl text-xl tracking-widest leading-none group-hover:text-dodoWhite-300 transition duration-200`} onClick={OneSignal.Notifications.requestPermission}>
+                  HAZ CLICK PARA NO PERDERTE LAS OBRAS
+                </button>
+      </div>
+    </section>
+  );
+}
