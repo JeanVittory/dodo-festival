03fdd0261c72fa47e2c5370c35da2c0b6b15ed09
Kofi <107807110+Kof27@users.noreply.github.com>
2025-03-14 20:04:08 -0500

    Update OneSignal integration and enhance push notification button component

--- src/components/bottonPushNotification/BottonPushNotiftication.tsx
+++ src/components/bottonPushNotification/BottonPushNotiftication.tsx
@@ -1,26 +1,33 @@
 "use client";
 
 import{useEffect} from 'react';
-import Onesignal from 'react-onesignal';
+import OneSignal from 'react-onesignal';
 
 import { nunito700 } from "@/lib/fonts"
+const oneSignalID = process.env.NEXT_PUBLIC_ONE_SIGNAL_APP_ID;
 
-export default function BottonPushNotification(){
-    return (
-        <div className="mt-0 mb-20 flex flex-col items-center">
-            {/* Inline-block container to shrink-wrap around the button */}
-            <div className="relative inline-block group">
-                {/* Gradient background */}
-                <div
-                    className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-400 rounded-2xl blur-lg opacity-75 group-hover:opacity-100 transition duration-200"
-                ></div>
-                {/* Actual button */}
-                <button
-                    className={`${nunito700.className} relative bg-dodoRed-100 py-2 px-5 rounded-2xl text-xl tracking-widest leading-none text-dodoWhite-200 group-hover:text-dodoWhite-300 transition duration-200`}
-                >
-                    HAZ CLICK PARA RECIBIR NOTIFICACIONES
-                </button>
-            </div>
+
+export default function Page() {
+    useEffect(() => {
+        // Ensure this code runs only on the client side
+        if (typeof window !== 'undefined') {
+          OneSignal.init({
+            appId: oneSignalID!,
+            // You can add other initialization options here
+            notifyButton: {
+              enable: true,
+            },
+            // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
+            allowLocalhostAsSecureOrigin: true
+          });
+        }
+      }, []);
+    
+      return (
+        <div>
+          <style jsx global>{`
+            ${nunito700}
+          `}</style>
         </div>
-    );
-}
+      );
+    }
